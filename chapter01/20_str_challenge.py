e3619b5eea7db591f3640b780d1c9d2a9d35f5b3
Dimi Serafeimidis <d.serafeimidis@outlook.com>
2026-01-20 09:10:53 +0200

    20/02/2026 1st commit

--- chapter01/20_str_challenge.py
+++ chapter01/20_str_challenge.py
@@ -9,3 +9,17 @@ str = "World"
 
 for i in range(len(str)):
     print(str[i] * (i + 1))
+
+
+# Hello
+# H***
+# ee**
+# lll*
+# 0000
+
+str = "Hello"
+
+
+for j in range(len(str)):
+    print(str[j] * (j + 1), end="*" * (len(str) - j - 1))
+    print()
